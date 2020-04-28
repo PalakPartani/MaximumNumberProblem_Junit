f78d854f4ac00cfbd43615ea6b2c265d242ade07
PalakPartani <partanipalak16@gmail.com>
2020-04-27 21:33:02 -0700

    REFACTORED:code to create Generic class to take variables of Generic type

--- src/main/java/com/bridgelabz/maximumnumber/MaximumNumber.java
+++ src/main/java/com/bridgelabz/maximumnumber/MaximumNumber.java
@@ -1,7 +1,37 @@
 package com.bridgelabz.maximumnumber;
 
-public class MaximumNumber {
-    public <E extends Comparable<E>> E findMaximum(E firstValue, E secondValue, E thirdValue) {
+public class MaximumNumber<E extends Comparable<E>> {
+    private E firstValue;
+    private E secondValue;
+    private E thirdValue;
+
+    public MaximumNumber(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public E findMaximum() {
+        return findMaximum(firstValue, secondValue, thirdValue);
+    }
+
+    public static <E extends Comparable<E>> E findMaximum(E firstValue, E secondValue, E thirdValue) {
+        E maxValue = firstValue;
+        if (maxValue.compareTo(secondValue) < 0) {
+            maxValue = secondValue;
+        }
+        if (maxValue.compareTo(thirdValue) < 0) {
+            maxValue = thirdValue;
+        }
+        //  printMax(maxValue);
+        return maxValue;
+    }
+
+   /* public static <E extends Comparable<E>> void printMax(E maxValue) {
+        System.out.println("Maximum value: " + maxValue);
+    }*/
+}
+ /*   public <E extends Comparable<E>> E findMaximum(E firstValue, E secondValue, E thirdValue) {
         E maxNumber = firstValue;
         if (maxNumber.compareTo(secondValue) < 0) {
             maxNumber = secondValue;
@@ -10,6 +40,4 @@ public class MaximumNumber {
             maxNumber = thirdValue;
         }
         return maxNumber;
-    }
-}
-
+    }*/
--- src/test/java/com/bridgelabz/maximumnumber/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/maximumnumber/MaximumNumberTest.java
@@ -1,72 +1,66 @@
 package com.bridgelabz.maximumnumber;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MaximumNumberTest {
-    MaximumNumber maximumNumber;
-
-    @Before
-    public void setUp() throws Exception {
-        maximumNumber = new MaximumNumber();
-    }
 
     //uc1
     @Test
     public void givenMaximumIntegerNumber_AtFirstPosition_ShouldReturnSameNumber() {
-        int max = maximumNumber.findMaximum(5, 3, 1);
-        Assert.assertEquals(5, max);
-
+        int max = new  MaximumNumber<>(50,20,10).findMaximum();
+        Assert.assertEquals(50, max);
     }
 
     @Test
     public void givenMaximumIntegerNumber_AtSecondPosition_ShouldReturnSameNumber() {
-        int max = maximumNumber.findMaximum(20, 50, 10);
+
+        int max = new  MaximumNumber<>(20,50,10).findMaximum();
         Assert.assertEquals(50, max);
     }
 
     @Test
     public void givenMaximumIntegerNumber_AtThirdPosition_ShouldReturnSameNumber() {
-        int max = maximumNumber.findMaximum(50, 30, 80);
+        int max = new  MaximumNumber<>(20,50,80).findMaximum();
         Assert.assertEquals(80, max);
     }
 
     //uc2
     @Test
     public void givenMaximumFloatNumber_AtFirstPosition_ShouldReturnSameNumber() {
-        Float result = maximumNumber.findMaximum(60.2f, 20.2f, 40.2f);
-        Assert.assertEquals(Float.valueOf(60.2f), result);
+        float max=new MaximumNumber<>(50.2f, 20.2f, 30.2f).findMaximum();
+        Assert.assertEquals(50.2f,max,0.0);
+
     }
 
     @Test
     public void givenMaximumFloatNumber_AtSecondNumber_ShouldReturnSameNumber() {
-        Float result = maximumNumber.findMaximum(20.f, 60.2f, 40.2f);
-        Assert.assertEquals(Float.valueOf(60.2f), result);
+        float result = new MaximumNumber<>(20.f, 60.2f, 40.2f).findMaximum();
+        Assert.assertEquals(60.2f, result,0.0);
     }
 
     @Test
     public void givenMaximumNumber_AtThirdNumber_ShouldReturnSameNumber() {
-        Float result = maximumNumber.findMaximum(40.2f, 20.2f, 60.2f);
-        Assert.assertEquals(Float.valueOf(60.2f), result);
+        float result = new MaximumNumber<>(20.f, 60.2f, 60.2f).findMaximum();
+        Assert.assertEquals(60.2f, result,0.0);
     }
 
+    //uc3
     @Test
     public void givenStringValue_whenAtFirstPosition_ShouldReturnMaxString() {
-        String result = maximumNumber.findMaximum("Plk", "Aaa", "Ccc");
+        String result = new MaximumNumber<>("Plk", "Aaa", "Ccc").findMaximum();
         Assert.assertEquals("Plk", result);
     }
 
     @Test
     public void giveStringValue_whenAtSecondPosition_ShouldReturnMaxString() {
-        String result = maximumNumber.findMaximum("Aaa", "Plk", "Ccc");
+        String result = new MaximumNumber<>("Aaa", "Plk", "Ccc").findMaximum();
         Assert.assertEquals("Plk", result);
     }
 
     @Test
     public void givenStringValue_whenAtThirdPosition_ShouldReturnMaxString() {
-        String result = maximumNumber.findMaximum("Aaa", "Cdd", "plk");
+        String result = new MaximumNumber<>("Aaa", "Cdd", "plk").findMaximum();
         Assert.assertEquals("plk", result);
     }
-
 }
